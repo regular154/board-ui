9e1a4f175fe8d6f6f2438d4bc526b657ba20546f
siroshtan <siroshtan.home@gmail.com>
2020-06-08 19:33:37 +0300

    add filter item component

---
+++ src/app/filter-item/filter-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-item',
+  templateUrl: './filter-item.component.html',
+  styleUrls: ['./filter-item.component.css']
+})
+export class FilterItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
